8e3e972bb725a2d3d21f4a66feb7901c94276512
poojapatidar21 <poojapatidar897@gmail.com>
2022-06-29 21:57:32 +0530

    error solved 6

--- dist/index.js
+++ dist/index.js
@@ -9009,8 +9009,6 @@ async function run() {
     const GITHUB_TOKEN=core.getInput('GITHUB_TOKEN');
     const octokit=github.getOctokit(GITHUB_TOKEN);
 
-    
-    console.log(pull_request);
     await octokit.rest.issues.createComment({
         ...context.repo,
         issue_number:context.payload.pull_request.number,
--- src/action.js
+++ src/action.js
@@ -8,8 +8,6 @@ async function run() {
     const GITHUB_TOKEN=core.getInput('GITHUB_TOKEN');
     const octokit=github.getOctokit(GITHUB_TOKEN);
 
-    
-    console.log(pull_request);
     await octokit.rest.issues.createComment({
         ...context.repo,
         issue_number:context.payload.pull_request.number,
